82ad4118d762212e7fbe16be3ec5ce935e2c548d
[fredok]gitconfig --global user.email [fredericksonde@gmail.com] <frederick.sonder@laplateforme.io>
2022-07-28 13:56:53 +0200

    modif favoris

--- vue/profil.php
+++ vue/profil.php
@@ -1,5 +1,6 @@
 <?= require 'header.php'; 
-    require '../model/user.php'; ?>
+    require '../model/user.php';
+?>
 <h3>Profil</h3>
     <div class= 'form'>
         <form action=""method="post">
@@ -20,8 +21,19 @@
         $user -> updateuser();
     }
 ?>
-<div class='_info'>
 
-</div>
+<section class="container">
+    <div class="scroll_bloc hover">
+        <h3>Vos favoris</h3>
+        <article class="scroll_container favoris_film"></article>
+    </div>
+</section>
+
+<section class="container">
+    <div class="scroll_bloc hover">
+        <h3>Vos séries favorites</h3>
+        <article class="scroll_container favoris_serie"></article>
+    </div>
+</section>
 
 <?= require 'footer.php'; ?>
